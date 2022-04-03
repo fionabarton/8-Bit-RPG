3dafcf102c4b5bcbfa3b09d9f2bce83ac6035b81
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-04-03 15:09:44 -0400

    Add refactored code from 16-bit prototype

---
+++ Enemies/EnemyStats.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy Stats")]
+public class EnemyStats : ScriptableObject {
+	public int id;
+	public new string name;
+	public Sprite sprite;
+	public int HP; // set to maxHP in BattleInitiative.cs
+	public int MP; // set to maxMP in BattleInitiative.cs
+	public int maxHP;
+	public int maxMP;
+	public int STR;
+	public int DEF;
+	public int WIS;
+	public int AGI;
+	public int EXP;
+	public int Gold;
+	public int LVL;
+
+	// Drop Item
+	public List<eItem> itemsToDrop = new List<eItem>();
+	public float chanceToDrop;
+	public int maxAmountToSteal; // amount of items that can be stolen from this enemy
+	public int amountToSteal; // set to maxAmountToSteal in BattleInitiative.cs
+
+	// Items stolen from party
+	public List<Item> stolenItems = new List<Item>();
+
+	// AI
+	public eEnemyAI AI;
+
+	// QuestNdx (If == 0, doesn't progress story) && doesn't StartBattle OnCollision
+	public int questNdx;
+
+	// Default Move
+	public float chanceToCallMove;
+	public int defaultMove;
+
+	// Actions/Moves Enemy can perform
+	public List<int> moveList;
+
+	public int animNdx;
+
+	public bool isDead; // set to false in BattleInitiative.cs
+}
