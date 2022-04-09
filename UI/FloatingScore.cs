ec277e14eb57666a92f3d97eb20020d0c5692bfc
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-04-09 18:40:09 -0400

    Add files via upload

---
+++ UI/FloatingScore.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingScore : MonoBehaviour {
+	[Header ("Set in Inspector")]
+	public float 	speed = 2f;
+
+	[Header ("Set Dynamically")]
+	private bool	canScale;
+
+	void OnEnable(){
+		StartCoroutine("FixedUpdateCoroutine");
+	}
+
+	void OnDisable(){
+		transform.localPosition = Vector3.zero;
+		transform.localScale = Vector3.one;
+	}
+
+	public IEnumerator FixedUpdateCoroutine () {
+		Vector3 tPos = transform.localPosition;
+		tPos.y += speed * Time.fixedDeltaTime;
+		transform.localPosition = tPos;
+
+		Invoke ("ScaleUp", 1.0f);
+		if (canScale) {
+			transform.localScale += (Vector3.one * Time.fixedDeltaTime)/5;
+		}
+
+		yield return new WaitForFixedUpdate ();
+		StartCoroutine("FixedUpdateCoroutine");
+	}
+
+	void ScaleUp() {
+		canScale = true;
+	}
+}
