0852e25a1fb4f0387961bf3d9d9c4a7bf5ed86be
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-04-09 18:39:34 -0400

    Add files via upload

---
+++ DestroyOverTime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour {
+	[Header ("Set in Inspector")]
+	public bool 	destroyRatherThanSetActive;
+	public float 	timeDuration = 1f;
+
+	[Header ("Set Dynamically")]
+	private float 	timeDone;
+
+	void OnEnable () {
+		timeDone = timeDuration + Time.time;
+
+		StartCoroutine ("FixedUpdateCoroutine");
+	}
+
+	public IEnumerator FixedUpdateCoroutine () {	
+		if (timeDone <= Time.time) {
+			if (destroyRatherThanSetActive) {
+				Destroy (gameObject);
+			} else {
+				gameObject.SetActive (false);
+			}
+		}
+
+		yield return new WaitForFixedUpdate ();
+		StartCoroutine ("FixedUpdateCoroutine");
+	}
+}
