8323f972d38396e93fe2052b1699688032190bec
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-04-15 19:01:36 -0400

    Add flicker when party poisoned

--- Blob.cs
+++ Blob.cs
@@ -10,7 +10,9 @@ public class Blob : MonoBehaviour {
 	public LayerMask	bounds;
 
 	[Header("Set Dynamically")]
-	public Animator		anim;
+	public Animator			anim;
+	public SpriteRenderer	sRend;
+	public Flicker			flicker;
 
 	private float		speed = 3f;
 	private bool		facingRight = true;
@@ -20,15 +22,19 @@ public class Blob : MonoBehaviour {
 	public float		destination;
 	public bool			alreadyTriggered; // Prevents triggering multiple triggers
 
-	private bool		canEncounter = true;
-	private int			encounterRate = 24;
+	public bool			isBattling = false;
+	public bool			canEncounter = true;
+	public int			encounterRate = 24;
+	public List<EnemyStats> enemyStats;
+	// Amount of enemies to battle. If 999, set to a random amount
+	public int			enemyAmount = 999;
 
-	// Singleton
+	public int			stepCount = 0;
+
 	private static Blob _S;
 	public static Blob S { get { return _S; } set { _S = value; } }
 
 	void Awake() {
-		// Singleton
 		S = this;
 
 		// DontDestroyOnLoad
@@ -44,6 +50,8 @@ public class Blob : MonoBehaviour {
 		UpdateManager.fixedUpdateDelegate += FixedLoop;
 
 		anim = GetComponent<Animator>();
+		sRend = GetComponent<SpriteRenderer>();
+		flicker = GetComponent<Flicker>();
 	}
 
     void Start() {
@@ -64,8 +72,12 @@ public class Blob : MonoBehaviour {
 						// Reposition movePoint horizontally
 						movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal") / 2f, 0f, 0f);
 
+						stepCount += 1;
+
 						// Check for random encounter 
 						CheckForRandomEncounter();
+
+						CheckForPoisonDamage();
 					}
 
 					// Set anim and trigger position
@@ -78,8 +90,12 @@ public class Blob : MonoBehaviour {
 						// Reposition movePoint vertically
 						movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical") / 2f, 0f);
 
+						stepCount += 1;
+
 						// Check for random encounter 
 						CheckForRandomEncounter();
+
+						CheckForPoisonDamage();
 					}
 
 					// Set anim and trigger position
@@ -100,22 +116,45 @@ public class Blob : MonoBehaviour {
 		if (canEncounter) {
 			if(Random.Range(0, encounterRate) == 0) {
 				// Start battle
-				Debug.Log("Start battle");
-
-				// Freeze player
-				canMove = false;
-				alreadyTriggered = true;
-
-				// Activate battle UI
-
-
-				// Set enemy stats
-
-
-				// Get turn order
-
+				StartCoroutine("StartBattle");
 			}
 		}
+	}
+
+	public IEnumerator StartBattle() {
+		// Set enemy stats
+		Battle.S.ImportEnemyStats(enemyStats, enemyAmount);
+
+		// Freeze player
+		canMove = false;
+		alreadyTriggered = true;
+
+		// Close curtain
+		Curtain.S.Close();
+
+		// Audio: Start Battle
+		AudioManager.S.PlaySong(eSongName.startBattle);
+
+		// Yield
+		yield return new WaitForSeconds(1.5f);
+
+		isBattling = true;
+
+		// Activate battle UI and gameobjects
+		Battle.S.UI.battleMenu.SetActive(true);
+		Battle.S.UI.battleGameObjects.SetActive(true);
+
+		Battle.S.InitializeBattle();
+
+		// Open curtain
+		Curtain.S.Open();
+
+		// Add Update & Fixed Update Delegate
+		UpdateManager.updateDelegate += Battle.S.Loop;
+		UpdateManager.fixedUpdateDelegate += Battle.S.FixedLoop;
+
+		// Audio: Ninja
+		AudioManager.S.PlaySong(eSongName.ninja);
 	}
 
 	void FixedLoop() {
@@ -123,6 +162,33 @@ public class Blob : MonoBehaviour {
 		if (Input.GetAxisRaw("Horizontal") > 0 && !facingRight ||
 			Input.GetAxisRaw("Horizontal") < 0 && facingRight) {
 			Utilities.S.Flip(gameObject, ref facingRight);
+		}
+	}
+
+	void CheckForPoisonDamage() {
+		// Every 4 steps...
+		if (!flicker.isInvincible) {
+			if (stepCount % 4 == 0) {
+				// For each party member...
+				for (int i = 0; i <= Party.S.partyNdx; i++) {
+					// If poisoned...
+					if (StatusEffects.S.CheckIfPoisoned(true, i)) {
+						// ...decrement HP by 1
+						if (Party.S.stats[i].HP > 1) {
+							Party.S.stats[i].HP -= 1;
+						}
+
+						// Audio: Damage
+						AudioManager.S.PlayRandomDamageSFX();
+
+						// Start flickering
+						flicker.StartInvincibility(0.5f, 0.1f, false);
+
+						// Display Floating Score
+						GameManager.S.InstantiateFloatingScore(gameObject, "-1", Color.red);
+					}
+				}
+			}
 		}
 	}
 }
