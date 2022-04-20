96bf63412bb030418dbc166a573468f5e6bbb752
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-04-20 18:32:45 -0400

    Add battle trigger
    Enables random encounters and sets encounter rate on player collision.

---
+++ Triggers/ActivateOnCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateOnCollision : MonoBehaviour {
+	protected virtual void OnTriggerEnter2D(Collider2D coll) {
+		if (!GameManager.S.paused) {
+			if (coll.gameObject.CompareTag("PlayerTrigger")) {
+				// Update Delgate
+				UpdateManager.updateDelegate += Loop;
+
+				Action();
+			}
+		}
+	}
+
+	protected virtual void OnTriggerExit2D(Collider2D coll) {
+		if (coll.gameObject.CompareTag("PlayerTrigger")) {
+			// Update Delgate
+			UpdateManager.updateDelegate -= Loop;
+		}
+	}
+
+	public void Loop() {
+
+	}
+
+	protected virtual void Action() {
+
+	}
+}
---
+++ Triggers/BattleTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleTrigger : ActivateOnCollision {
+    [Header("Set in Inspector")]
+    public bool canEncounter = false;
+
+    public int encounterRate = 24;
+
+    protected override void Action() {
+        // Enable random encounters
+        Blob.S.canEncounter = canEncounter;
+
+        // Set encounter rate
+        Blob.S.encounterRate = encounterRate;
+    }
+}
