28afe6b1bcdd4237bbd02bbe27e1055df9253a35
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-04-22 14:44:43 -0400

    Get list of randomly selected enemies
    Returns a list of randomly selected enemies based on the player’s current location.

---
+++ Managers/EnemyManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public List<EnemyStats> enemies0;
+    public List<EnemyStats> enemies1;
+
+	public List<EnemyStats> GetEnemies(int locationNdx) {
+        // Clear enemy list
+        Blob.S.enemyStats.Clear();
+
+        // Declare local variables
+        List<EnemyStats> tEnemies = new List<EnemyStats>();
+        int randomNdx;
+
+        // Populate list of randomly selected enemies from this location
+        switch (locationNdx) {
+            case 0:
+                for(int i = 0; i < 5; i++) {
+                    randomNdx = Random.Range(0, enemies0.Count);
+                    tEnemies.Add(enemies0[randomNdx]);
+                }
+                break;
+            case 1:
+                for (int i = 0; i < 5; i++) {
+                    randomNdx = Random.Range(0, enemies1.Count);
+                    tEnemies.Add(enemies1[randomNdx]);
+                }
+                break;
+        }
+
+        // Return list of randomly selected enemies from this location
+        return tEnemies;
+    } 
+}
