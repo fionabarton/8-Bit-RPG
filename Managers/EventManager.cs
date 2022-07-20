c7cbcda208556ee9ccec5f86c8ee6a10eee62c3c
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-07-20 10:35:57 -0400

    Add files via upload

---
+++ Managers/EventManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* This script is NOT referenced in any scene (attached to a game object)
+ * 
+ * It's referenced in the following scripts:
+ * ActivateOnButtonPress, EventManager, ShopkeeperTrigger, ShopMenu
+ */
+public class EventManager : MonoBehaviour {
+    public delegate void ReactivateShopkeeperTriggerAction();
+    public static event ReactivateShopkeeperTriggerAction OnShopScreenDeactivated;
+
+    public static void ShopScreenDeactivated() {
+        OnShopScreenDeactivated?.Invoke();
+    }
+}
