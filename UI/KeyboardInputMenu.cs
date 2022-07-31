52ec11a315d59e1c85660d483718244062cdaf6d
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-07-31 11:51:27 -0400

    Update KeyboardInputMenu.cs
    - Add individual Text object for each letter of displayed name text
    - Add underline underneath each letter of displayed name text
    - Add shake name input animation if can’t add/remove a char

--- UI/KeyboardInputMenu.cs
+++ UI/KeyboardInputMenu.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
@@ -7,19 +6,21 @@ using System.Linq;
 // Handles creating character names via user keyboard input
 public class KeyboardInputMenu : MonoBehaviour {
 	[Header("Set in Inspector")]
+	// List of buttons for each letter, number, symbol, etc. input
 	public List<GameObject> buttonsGO;
 
-	[Header("Set Dynamically")]
-	public Text inputBoxText;
-
-	public string inputString;
+	// Each slots represents one char 
+	public List<Text> charSlotsText;
 
 	public GameObject startingSelectedGO;
 
-	// 0-25, 26-51, 52-61, 62-65, 66-69, 70-89
+	[Header("Set Dynamically")]
+	private string inputString = "";
+
+	// Uppercase: 0-25, Lowercase: 26-51, Numbers: 52-61, Symbols: 62-65, 66-69, 70-89
 	private string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789()[]!?.,~@#$%^&*+-=_\"'` :;/\\";
 
-	// 
+	// Variables related to predetermined default names
 	private int dontCareNdx;
 	private List<string> dontCareNames = new List<string>() { "Butthead", "Mildew", "Gunt", "Love Gum", "Moon Unit" };
 
@@ -29,11 +30,15 @@ public class KeyboardInputMenu : MonoBehaviour {
 	// Ensures audio is only played once when button is selected
 	public GameObject previousSelectedGameObject;
 
+	private Animator inputBoxAnim;
+
 	private static KeyboardInputMenu _S;
 	public static KeyboardInputMenu S { get { return _S; } set { _S = value; } }
 
 	void Awake() {
 		S = this;
+
+		inputBoxAnim = GetComponentInChildren<Animator>();
 	}
 
 	void Start() {
@@ -103,38 +108,44 @@ public class KeyboardInputMenu : MonoBehaviour {
 			canUpdate = false;
 		}
 
-		// Backspace
-		if (Input.GetButtonDown("SNES Y Button")) {
+		// Backspace input
+		if (Input.GetButtonDown("SNES Y Button") || Input.GetKeyDown(KeyCode.Backspace)) {
 			Backspace();
 		}
 
-		// Space
+		// Space input
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			PressedKey(85);
 		}
 	}
 
+	// Add a char to the displayed name
 	public void PressedKey(int ndx) {
 		if (inputString.Length < 15) {
 			for (int i = 0; i < characters.Length; i++) {
 				if (i == ndx) {
 					// Add user keyboard input to string
 					inputString += characters[ndx];
-					inputBoxText.text = inputString + GetRemainingChars();
+					DisplayText(inputString + GetRemainingWhitespace());
 
 					// Audio: Confirm
 					AudioManager.S.PlaySFX(eSoundName.confirm);
 				}
 			}
         } else {
+			// Input box shake animation
+			inputBoxAnim.CrossFade("Shake", 0);
+
 			// Audio: Deny
 			AudioManager.S.PlaySFX(eSoundName.deny);
 		}
 	}
 
+	// Sets the displayed name to a predetermined default name
 	public void DontCare() {
+		// Get a default name
 		inputString = dontCareNames[dontCareNdx];
-		inputBoxText.text = inputString + GetRemainingChars();
+		DisplayText(inputString + GetRemainingWhitespace());
 
 		// Increment index
 		if (dontCareNdx < dontCareNames.Count - 1) {
@@ -147,11 +158,15 @@ public class KeyboardInputMenu : MonoBehaviour {
 		AudioManager.S.PlaySFX(eSoundName.confirm);
 	}
 
+	// Remove the last char from the displayed name
 	public void Backspace() {
 		if(inputString.Length > 0) {
 			// Remove last char from the string
 			inputString = inputString.Remove(inputString.Length - 1);
-			inputBoxText.text = inputString + GetRemainingChars();
+			DisplayText(inputString + GetRemainingWhitespace());
+        } else {
+			// Input box shake animation
+			inputBoxAnim.CrossFade("Shake", 0);
 		}
 
 		// Audio: Deny
@@ -162,20 +177,28 @@ public class KeyboardInputMenu : MonoBehaviour {
 
     }
 
-	// Returns a string of remaining chars
-	public string GetRemainingChars() {
+	// Returns a string of whitespace as long as the amount of remaining empty chars 
+	public string GetRemainingWhitespace() {
 		string remainingChars = "";
 		if (inputString.Length < 15) {
-			// Get amount of remaing space
+			// Get amount of remaining space
 			int amountOfRemainingChars = 15 - inputString.Length;
 
+			// Populate string with whitespace 
 			for (int j = 0; j < amountOfRemainingChars; j++) {
-				remainingChars += "_";
+				remainingChars += " ";
 			}
 		}
 
 		return remainingChars;
 	}
+
+	// Display each char of the 'text' argument in an individual Text object
+	public void DisplayText(string text) {
+		for(int i = 0; i < charSlotsText.Count; i++) {
+			charSlotsText[i].text = text[i].ToString();
+		}
+    }
 
 	//  private void Update() {
 	//foreach (char cIt in Input.inputString) {
