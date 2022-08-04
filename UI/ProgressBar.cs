67e5c788bafcac256da18607c666b1391a2d0c90
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-08-03 22:02:13 -0400

    Add enemy health bars

---
+++ UI/ProgressBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameObject))]
+public class ProgressBar : MonoBehaviour {
+	[Header("Set in Inspector")]
+	public GameObject	currentBar;
+
+	[Header("Set Dynamically")]
+	float				progressBarMaxWidth;
+	SpriteRenderer		sRend;
+
+	void Awake(){
+		progressBarMaxWidth = currentBar.transform.localScale.x;
+
+		sRend = GetComponent<SpriteRenderer>();
+	}
+
+	public void UpdateBar(float currentHP, float maxHP) {
+		// Scale
+		Vector3 scale = currentBar.transform.localScale;
+		scale.x = Utilities.S.Map(0, maxHP, 0, progressBarMaxWidth, currentHP);
+		currentBar.transform.localScale = scale;
+
+		// Position
+		Vector3 pos = currentBar.transform.localPosition;
+		pos.x = (scale.x / 2) - 0.5625f;
+		currentBar.transform.localPosition = pos;
+
+		// Color
+		if(currentHP >= ((maxHP / 3) * 2))
+        {
+			sRend.color = Color.green;
+        }
+		else if (currentHP >= (maxHP / 3))
+        {
+			sRend.color = Color.yellow;
+		}
+        else
+        {
+			sRend.color = Color.red;
+		}
+	}
+}
---
+++ UI/ProgressBars.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Provides access to both the party and enemy's battle progress bars
+/// </summary>
+public class ProgressBars : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    // Health Bars
+    public List<ProgressBar> playerHealthBarsCS;
+    public List<ProgressBar> enemyHealthBarsCS;
+
+    // Magic Bars
+    public List<ProgressBar> playerMagicBarsCS;
+
+    [Header("Set Dynamically")]
+    private static ProgressBars _S;
+    public static ProgressBars S { get { return _S; } set { _S = value; } }
+
+    void Awake() {
+        S = this;
+    }
+}
