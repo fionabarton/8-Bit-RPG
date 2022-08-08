8e7cbea0db47938cbfb3ac910344c9ed103e7e1a
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-08-07 20:49:44 -0400

    Add files via upload

---
+++ Triggers/SpriteSwapTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used for swaping open/closed door sprites on collision
+/// </summary>
+public class SpriteSwapTrigger : MonoBehaviour {
+	[Header("Set in Inspector")]
+	public List<Sprite> sprites = new List<Sprite>();
+
+	public bool swapOnlyOnFirstCollision = true;
+
+	public eSoundName soundName;
+
+	public SpriteRenderer sRend;
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (swapOnlyOnFirstCollision) {
+			if (coll.gameObject.CompareTag("PlayerTrigger")) {
+				sRend.sprite = sprites[0];
+
+				AudioManager.S.PlaySFX(soundName);
+
+				swapOnlyOnFirstCollision = false;
+			}
+		}
+	}
+}
