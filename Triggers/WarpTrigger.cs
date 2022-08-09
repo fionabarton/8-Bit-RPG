0e2515818c78c42a817d1011641a6853b52f86c2
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-08-09 16:15:17 -0400

    Add warp trigger

---
+++ Triggers/WarpTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WarpTrigger : MonoBehaviour {
+	[Header("Set in Inspector")]
+	public bool warpOnContact;
+
+	// New Scene
+	public bool warpToNewScene;
+	public string sceneName;
+
+	// Player Pos
+	public Vector3 playerWarpPos;
+
+	// Camera
+	public bool camFollows;
+	public Vector3 camWarpPos;
+
+	// Warp on contact
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (warpOnContact) {
+			if (coll.gameObject.CompareTag("PlayerTrigger")) {
+				StartCoroutine(WarpManager.S.Warp(playerWarpPos, warpToNewScene, sceneName, camFollows, camWarpPos));
+			}
+		}
+	}
+
+	// Warp on Button Press
+	void OnTriggerStay2D(Collider2D coll) {
+		if (coll.gameObject.CompareTag("Player")) {
+			//Blob.S.rigid.sleepMode = RigidbodySleepMode2D.NeverSleep;
+
+			if (Input.GetButtonDown("SNES B Button")) {
+				StartCoroutine(WarpManager.S.Warp(playerWarpPos, warpToNewScene, sceneName, camFollows, camWarpPos));
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D coll) {
+		if (coll.gameObject.CompareTag("Player")) {
+			//Player.S.rigid.sleepMode = RigidbodySleepMode2D.StartAwake;
+		}
+	}
+}
