5498df2c153e9e6748ea35d95728cc3df54c990d
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-08-08 20:27:18 -0400

    Add files via upload

---
+++ World Objects/Don'tDestroyOnLoad/Follower1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Follower1 : MonoBehaviour {
+    [Header("Set Dynamically")]
+    private static bool exists;
+
+    void Awake() {
+        // DontDestroyOnLoad
+        if (!exists) {
+            exists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ World Objects/Don'tDestroyOnLoad/Follower2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Follower2 : MonoBehaviour {
+    [Header("Set Dynamically")]
+    private static bool exists;
+
+    void Awake() {
+        // DontDestroyOnLoad
+        if (!exists) {
+            exists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
