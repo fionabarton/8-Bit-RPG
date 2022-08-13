d8a8f5e8f411e603bc4c32c423bccbdadc9c68e8
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-08-13 10:28:26 -0400

    Add indicate player turn by flashing stat frame

---
+++ World Objects/SetAnimatorSpeed.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SetAnimatorSpeed : MonoBehaviour {
+    [Header("Set dynamically")]
+    public Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    public void SetSpeed(int speed = 0) {
+        anim.speed = speed;
+    }
+}
