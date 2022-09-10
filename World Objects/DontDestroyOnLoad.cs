388ed9d1f2675f705fd1d02ceb67920863b22b16
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-09-10 14:43:26 -0400

    Add files via upload

---
+++ World Objects/DontDestroyOnLoad.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+    [Header("Set Dynamically")]
+    private bool exists;
+
+    void Awake() {
+        // DontDestroyOnLoad
+        if (!exists) {
+            exists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ World Objects/SetActiveOnStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveOnStart : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public bool activateOnStart = true;
+
+    void Start() {
+        gameObject.SetActive(activateOnStart);
+    }
+}
