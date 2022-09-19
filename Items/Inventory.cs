caf50769ef33ed3363613d29c6f8c50dfdde071a
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-09-19 10:52:02 -0400

    Add save/load party inventory

--- Items/Inventory.cs
+++ Items/Inventory.cs
@@ -18,51 +18,51 @@ public class Inventory : MonoBehaviour {
 
     void Start() {
         // Add items to inventory
+        //AddItemToInventory(Items.S.items[2]);
+        //AddItemToInventory(Items.S.items[8]);
+        //AddItemToInventory(Items.S.items[9]);
         //AddItemToInventory(Items.S.items[25]);
-        AddItemToInventory(Items.S.items[25]);
-        AddItemToInventory(Items.S.items[25]);
+        //AddItemToInventory(Items.S.items[29]);
+
+        //AddItemToInventory(Items.S.items[25]);
+        //AddItemToInventory(Items.S.items[25]);
+        //AddItemToInventory(Items.S.items[25]);
+        ////AddItemToInventory(Items.S.items[26]);
         //AddItemToInventory(Items.S.items[26]);
-        AddItemToInventory(Items.S.items[26]);
-        AddItemToInventory(Items.S.items[26]);
-        AddItemToInventory(Items.S.items[27]);
-        AddItemToInventory(Items.S.items[27]);
+        //AddItemToInventory(Items.S.items[26]);
         //AddItemToInventory(Items.S.items[27]);
-        AddItemToInventory(Items.S.items[28]);
-        AddItemToInventory(Items.S.items[29]);
-        AddItemToInventory(Items.S.items[0]);
-        AddItemToInventory(Items.S.items[0]);
-        AddItemToInventory(Items.S.items[1]);
-
-        AddItemToInventory(Items.S.items[23]);
-        AddItemToInventory(Items.S.items[23]);
-        AddItemToInventory(Items.S.items[2]);
-        AddItemToInventory(Items.S.items[3]);
-        AddItemToInventory(Items.S.items[4]);
-        AddItemToInventory(Items.S.items[5]);
-        AddItemToInventory(Items.S.items[6]);
-        AddItemToInventory(Items.S.items[7]);
-        AddItemToInventory(Items.S.items[8]);
-        AddItemToInventory(Items.S.items[9]);
-
-        AddItemToInventory(Items.S.items[10]);
-        AddItemToInventory(Items.S.items[0]);
-        AddItemToInventory(Items.S.items[11]);
-        AddItemToInventory(Items.S.items[12]);
-        AddItemToInventory(Items.S.items[13]);
-        AddItemToInventory(Items.S.items[14]);
-        AddItemToInventory(Items.S.items[15]);
-        AddItemToInventory(Items.S.items[16]);
-        AddItemToInventory(Items.S.items[17]);
-        AddItemToInventory(Items.S.items[18]);
-        AddItemToInventory(Items.S.items[19]);
-        AddItemToInventory(Items.S.items[20]);
-        AddItemToInventory(Items.S.items[21]);
-
-        AddItemToInventory(Items.S.items[22]);
-        AddItemToInventory(Items.S.items[22]);
-        AddItemToInventory(Items.S.items[24]);
-        AddItemToInventory(Items.S.items[24]);
-        AddItemToInventory(Items.S.items[24]);
+        //AddItemToInventory(Items.S.items[27]);
+        ////AddItemToInventory(Items.S.items[27]);
+        //AddItemToInventory(Items.S.items[28]);
+        //AddItemToInventory(Items.S.items[29]);
+        //AddItemToInventory(Items.S.items[0]);
+        //AddItemToInventory(Items.S.items[0]);
+        //AddItemToInventory(Items.S.items[1]);
+        //AddItemToInventory(Items.S.items[23]);
+        //AddItemToInventory(Items.S.items[23]);
+        //AddItemToInventory(Items.S.items[3]);
+        //AddItemToInventory(Items.S.items[4]);
+        //AddItemToInventory(Items.S.items[5]);
+        //AddItemToInventory(Items.S.items[6]);
+        //AddItemToInventory(Items.S.items[7]);
+        //AddItemToInventory(Items.S.items[10]);
+        //AddItemToInventory(Items.S.items[0]);
+        //AddItemToInventory(Items.S.items[11]);
+        //AddItemToInventory(Items.S.items[12]);
+        //AddItemToInventory(Items.S.items[13]);
+        //AddItemToInventory(Items.S.items[14]);
+        //AddItemToInventory(Items.S.items[15]);
+        //AddItemToInventory(Items.S.items[16]);
+        //AddItemToInventory(Items.S.items[17]);
+        //AddItemToInventory(Items.S.items[18]);
+        //AddItemToInventory(Items.S.items[19]);
+        //AddItemToInventory(Items.S.items[20]);
+        //AddItemToInventory(Items.S.items[21]);
+        //AddItemToInventory(Items.S.items[22]);
+        //AddItemToInventory(Items.S.items[22]);
+        //AddItemToInventory(Items.S.items[24]);
+        //AddItemToInventory(Items.S.items[24]);
+        //AddItemToInventory(Items.S.items[24]);
     }
 
     public void AddItemToInventory(Item name) {
@@ -114,5 +114,62 @@ public class Inventory : MonoBehaviour {
             return items[name];
         }
         return 0;
+    }
+
+    // Load/save party's inventory ////////////////////////////////
+    ///////////////////////////////////////////////////////////////
+
+    // Save the party's inventory:
+    // Convert the party's inventory into a string of item ids
+    public string GetInventoryString() {
+        // Get list of item keys
+        List<Item> list = new List<Item>(items.Keys);
+
+        // Initialize string to store item ids
+        string inventoryString = "";
+
+        // Loop over each item key
+        for (int i = 0; i < list.Count; i++) {
+            // Get item amount
+            int itemCount = GetItemCount(list[i]);
+
+            // Add item id to inventorystring
+            for (int j = 0; j < itemCount; j++) {
+                inventoryString += list[i].id;
+            }
+        }
+
+        // Return string of item keys
+        return inventoryString;
+    }
+
+    // Load the party's inventory:
+    // Convert a string of item ids into the party's inventory
+    public void GetInventoryFromString(string inventoryString) {
+        // Clear current inventory 
+        items.Clear();
+
+        // Initialize string to temporarily store each item id
+        string itemId = "";
+
+        // Loop over string of item ids
+        for (int i = 0; i < inventoryString.Length; i++) {
+            // Build 3-char item id
+            itemId += inventoryString[i];
+
+            // Every 3rd char...
+            if ((i + 1) % 3 == 0) {
+                // If item id is valid...
+                for (int j = 0; j < Items.S.items.Length; j++) {
+                    if (itemId == Items.S.items[j].id) {
+                        // Add item to inventory
+                        AddItemToInventory(Items.S.items[j]);
+                    }
+                }
+
+                // Reset string to build next 3-char item id
+                itemId = "";
+            }
+        }
     }
 }
