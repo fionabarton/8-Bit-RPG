cd384329b6245b680e7f894a546f74098be4b263
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-09-24 20:55:08 -0400

    Add ‘mute’ switch to options menu

--- Managers/AudioManager.cs
+++ Managers/AudioManager.cs
@@ -120,10 +120,10 @@ public class AudioManager : MonoBehaviour {
 		// Sets 'Mute Audio' slider value on OptionsMenu,
 		// which calls PauseAndMuteAudio() when its value is changed
 		if (!AudioListener.pause) {
-            OptionsMenu.S.slidersGO[5].value = 1;
-        } else {
             OptionsMenu.S.slidersGO[5].value = 0;
-        }
+		} else {
+            OptionsMenu.S.slidersGO[5].value = 1;
+		}
     }
 
     public void PauseAndMuteAudio() {
