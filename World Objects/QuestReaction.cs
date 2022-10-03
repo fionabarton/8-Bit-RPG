593e64d4ddcac01fa44c086468527a29039b2459
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-10-03 18:54:30 -0400

    Add files via upload

---
+++ World Objects/QuestReaction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class QuestReaction : MonoBehaviour {
+	[Header ("Set in Inspector")]
+	// Indexes of which quests to react to
+	public int questIsCompletedNdx = -1;
+
+	// Actions to perform
+	public eQuestAction questIsCompletedAction;
+
+	// Deactivate GameObject if associated Quest has been Completed
+	void OnEnable() {
+		// If associated quest has been completed...
+		if (questIsCompletedNdx >= 0) {
+			if (QuestManager.S.quests[questIsCompletedNdx].isCompleted) {
+				switch (questIsCompletedAction) {
+					case eQuestAction.deactivateGo:
+						gameObject.SetActive(false);
+						break;
+					case eQuestAction.activateGo:
+						gameObject.SetActive(true);
+						break;
+				}
+            } else {
+				switch (questIsCompletedAction) {
+					case eQuestAction.deactivateGo:
+						gameObject.SetActive(true);
+						break;
+					case eQuestAction.activateGo:
+						gameObject.SetActive(false);
+						break;
+				}
+			}
+		}
+	}
+}
