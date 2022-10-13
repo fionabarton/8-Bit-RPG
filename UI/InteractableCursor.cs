6060ab7702e02eb1a9ac2f60c4586ec162599991
fionabarton <56236345+fionabarton@users.noreply.github.com>
2022-10-13 14:00:17 -0400

    Add Activate() with Vector2 argument

--- UI/InteractableCursor.cs
+++ UI/InteractableCursor.cs
@@ -1,30 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// Attached to RPGMainCamera; used to get InteractableCursorHolder gameObject
 /// </summary>
 public class InteractableCursor : MonoBehaviour{
-	[Header("Set Dynamically")]
-	// Singleton
-	private static InteractableCursor _S;
-	public static InteractableCursor S { get { return _S; } set { _S = value; } }
-	
 	[Header("Set in Inspector")]
 	// Cursor GameObject
 	public GameObject cursorGO;
+
+	[Header("Set Dynamically")]
+	private static InteractableCursor _S;
+	public static InteractableCursor S { get { return _S; } set { _S = value; } }
 
 	void Awake() {
 		S = this;
 	}
 
-	// Activate cursor gameObject
+	// Activate and position cursor gameObject
 	public void Activate (GameObject newParentGO) {
 		// Set position
 		Utilities.S.SetPosition(cursorGO, newParentGO.transform.position.x, newParentGO.transform.position.y + 0.5f);
 		// Set parent
 		cursorGO.transform.SetParent(newParentGO.transform);
+		// Activate gameObject
+		cursorGO.SetActive(true);	
+	}
+	public void Activate(Vector2 newPosition) {
+		// Set position
+		Utilities.S.SetPosition(cursorGO, newPosition.x, newPosition.y + 0.5f);
+		// Set parent
+		cursorGO.transform.SetParent(ObjectPool.S.gameObject.transform);
 		// Activate gameObject
 		cursorGO.SetActive(true);
 	}
