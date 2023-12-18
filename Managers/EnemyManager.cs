5f6540369530f29d9514bee280f2e06f8a7a972f
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-12-17 19:27:35 -0600

    Add examples of typical enemy groupings
    Add examples of typical enemy groupings:
    1) Randomly select enemies from a group of enemies.
    2) Explicitly select a specific enemy.
    3) Explicitly select a group of enemies.

--- Managers/EnemyManager.cs
+++ Managers/EnemyManager.cs
@@ -6,8 +6,9 @@ public class EnemyManager : MonoBehaviour {
     [Header("Set in Inspector")]
     public List<EnemyStats> enemies0;
     public List<EnemyStats> enemies1;
+    public List<EnemyStats> enemies2;
 
-	public List<EnemyStats> GetEnemies(int locationNdx) {
+    public List<EnemyStats> GetEnemies(int locationNdx) {
         // Clear enemy list
         Player.S.enemyStats.Clear();
 
@@ -19,15 +20,47 @@ public class EnemyManager : MonoBehaviour {
         switch (locationNdx) {
             case 0:
                 for(int i = 0; i < 5; i++) {
+                    // Randomly select enemies from this group
                     randomNdx = Random.Range(0, enemies0.Count);
                     tEnemies.Add(enemies0[randomNdx]);
                 }
+
+                // Randomize enemy amount
+                Player.S.enemyAmount = 999;
                 break;
             case 1:
                 for (int i = 0; i < 5; i++) {
+                    // Randomly select enemies from this group
                     randomNdx = Random.Range(0, enemies1.Count);
                     tEnemies.Add(enemies1[randomNdx]);
                 }
+
+                // Randomize enemy amount
+                Player.S.enemyAmount = 999;
+                break;
+            case 2:
+                // Get random float
+                float randomVal = Random.value;
+
+                // Randomly select a group of enemies
+                if(randomVal >= 0.5f) {
+                    for (int i = 0; i < 5; i++) {
+                        // Explicitly select enemies from this group
+                        tEnemies.Add(enemies0[i]);
+                    }
+
+                    // Explictly set enemy amount
+                    Player.S.enemyAmount = 1;
+                } else {
+                    for (int i = 0; i < 5; i++) {
+                        // Randomly select enemies from this group
+                        randomNdx = Random.Range(0, enemies2.Count);
+                        tEnemies.Add(enemies2[randomNdx]);
+                    }
+
+                    // Randomize enemy amount
+                    Player.S.enemyAmount = 999;
+                }              
                 break;
         }
 
